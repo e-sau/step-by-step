fda1f93c1d933196252b88336b84be1234aeadd2
cheefs <cheefst@outlook.com>
2020-10-19 21:56:25 +0700

    [*] наброски для будующего запросника, и перетащил мемо функцию, пригодится

---
+++ resources/js/src/common/memo.js
@@ -0,0 +1,21 @@
+/**
+ * Функция мемоизации ( кеширование на стороне клиента, кешируется до перезагрузки страници )
+ *
+ * @param { Function } fn
+ * @return { Function }
+ *
+ * @example Пример вызова функции, создаем переменную, и замыкаем то что хотим вызвать
+ *          const someMemo = memo( ( params ) => userApi.get( params ) );
+ **/
+export function memo( fn ) {
+    const memoCache = {};
+    return function () {
+        const key = JSON.stringify( arguments );
+        let value = memoCache[ key ] || null;
+        if ( !value ) {
+            value = fn.apply( null, arguments );
+            memoCache[ key ] = value;
+        }
+        return value;
+    }
+}
