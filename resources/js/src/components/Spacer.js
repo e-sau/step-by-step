185b38c6b104bbea20a8e47c271615ba6b533bc3
Daria Diachkova <daria.diachkova@mail.ru>
2020-11-21 10:00:35 +0300

    Refactor Spacer

--- resources/js/src/components/Spacer.js
+++ resources/js/src/components/Spacer.js
@@ -4,12 +4,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 export function Spacer( props ) {
-  const { size } = props;
-
   return (
-    <StyledSpacer>
-      <div className={`spacer_${size}`}></div>
-    </StyledSpacer>
+    <StyledSpacer size={ props.size } />
   )
 }
 
@@ -19,30 +15,10 @@ Spacer.defaultProps = {
 
 Spacer.propTypes = {
   size: PropTypes.oneOf(
-    [4, 8, 20, 32, 48, 64, 80]
+    [4, 8, 20, 24, 32, 48, 64, 80, 120]
   ).isRequired
 };
 
 const StyledSpacer = styled("div")`
-  .spacer_4 {
-    margin-bottom: 4px;
-  }
-  .spacer_8 {
-    margin-bottom: 8px;
-  }
-  .spacer_20 {
-    margin-bottom: 20px;
-  }
-  .spacer_32 {
-    margin-bottom: 32px;
-  }
-  .spacer_48 {
-    margin-bottom: 48px;
-  }
-  .spacer_64 {
-    margin-bottom: 64px;
-  }
-  .spacer_80 {
-    margin-bottom: 80px;
-  }
+  margin-bottom: ${props => props.size}px
 `;
