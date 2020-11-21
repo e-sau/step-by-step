5e39484b46a55f8e2c3e8137d8fa68b2e23e24d8
cheefs <cheefst@outlook.com>
2020-11-21 21:53:29 +0700

    [+] add features block

--- resources/js/src/components/indexPage/index.js
+++ resources/js/src/components/indexPage/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Preview from "./preview";
 import SubjectsSelectContainer from "../../containers/SubjectsSelectContainer";
+import {Features} from "./features";
 
 export default function ( props ) {
     return (
         <React.Fragment>
-            {/*<Preview/>*/}
+            <Preview/>
             <SubjectsSelectContainer/>
+            <Features/>
         </React.Fragment>
     );
 }
