a8b297978bbece0f1941c89edf49f6455bddb8a4
esau <esau@yandex.ru>
2020-11-21 19:04:58 +0300

    Changed Subject routes - excluded authorization on index and show methods

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,9 @@ Route::middleware('auth:api')->group(function() {
         Route::apiResource('roles', \App\Http\Controllers\Api\RoleController::class);
         Route::apiResource('schools', \App\Http\Controllers\Api\SchoolController::class);
         Route::apiResource('grades', \App\Http\Controllers\Api\GradeController::class);
-        Route::apiResource('subjects', \App\Http\Controllers\Api\SubjectController::class);
+        Route::apiResource('subjects', \App\Http\Controllers\Api\SubjectController::class, [
+            'except' => ['index', 'show']
+        ]);
         Route::apiResource('tasks', \App\Http\Controllers\Api\TaskController::class);
         Route::apiResource('achievements', \App\Http\Controllers\Api\AchievementController::class);
         Route::apiResource('additions', \App\Http\Controllers\Api\AdditionController::class);
@@ -41,6 +43,20 @@ Route::middleware('auth:api')->group(function() {
 Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::post('/login', \App\Http\Controllers\Api\Auth\AuthController::class.'@login')->name('login');
     Route::post('/register', \App\Http\Controllers\Api\Auth\AuthController::class.'@register')->name('register');
+
+/* Subjects */
+    Route::get('/subjects', \App\Http\Controllers\Api\SubjectController::class.'@index');
+
+    Route::get('/subjects/{slug}', function (Request $request, $slug) {
+        $subject = \App\Models\Subject::where(['slug' => $slug])->get()->first();
+        return (new \App\Http\Controllers\Api\SubjectController())->show($request, $subject);
+    })->where('slug', '[a-z]+');
+
+    Route::get('/subjects/{id}', function (Request $request, $id) {
+        $subject = \App\Models\Subject::where(['id' => $id])->get()->first();
+        return (new \App\Http\Controllers\Api\SubjectController())->show($request, $subject);
+    })->where('id', '[0-9]+');
+/* /Subjects */
 });
 
 Route::get('{path}', function () {
