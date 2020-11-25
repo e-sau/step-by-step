81db5bfd6c9485bd19472754368333eb05077d2f
cheefs <cheefst@outlook.com>
2020-11-25 21:28:58 +0700

    [jest] add test cases for common/helpers, update helpers code to resolve bugs

---
+++ resources/js/test/common/helpers.test.js
@@ -0,0 +1,93 @@
+import { object, string } from "../../src/common/helpers";
+
+describe("Testing objectHelpers", () => {
+
+    describe("testing update function", () => {
+        it("should to throw TypeError", function () {
+            expect( object.update ).toThrow( TypeError );
+            expect( () => {
+                object.update( null, []);
+            }).toThrow( TypeError );
+            expect( () => {
+                object.update( "string", []);
+            }).toThrow( TypeError );
+
+            expect( () => {
+                object.update({}, "string");
+            }).toThrow( TypeError );
+        });
+
+        it("should to return new ref", function () {
+            const testProperties = ["t", "newValue"];
+            const testObject = { key: "test", t: "" };
+
+            const clone = object.update( testObject, [
+                testProperties
+            ]);
+
+            expect( clone ).not.toBe( testObject );
+            expect( clone ).toStrictEqual({
+                ...testObject, [ testProperties[0] ]: testProperties[1]
+            });
+        });
+    });
+
+    describe("testing keysTransform function", () => {
+
+        it("should to throw Error", function () {
+            expect( object.keysTransform ).toThrow( Error );
+            expect(() => {
+                object.keysTransform( {} );
+            }).toThrow( Error );
+        });
+
+        it("should to throw TypeError", function () {
+            expect( () => {
+                object.keysTransform( null, () => 1 );
+            }).toThrow( TypeError );
+
+            expect( () => {
+                object.keysTransform({}, "string" );
+            }).toThrow( TypeError );
+        });
+
+        it("should to convertObject keys", function () {
+            const testObject = {
+                snake_case: "value",
+                another_snake_case: "anotherValue"
+            };
+            const transformedKeys = object.keysTransform( testObject, string.snakeCaseToCamelCase );
+
+            expect( transformedKeys ).not.toStrictEqual({
+                snakeCase: "value",
+                another_snake_case: "anotherValue"
+            });
+
+            expect( transformedKeys ).toStrictEqual({
+                snakeCase: "value",
+                anotherSnakeCase: "anotherValue"
+            });
+        });
+
+    });
+
+});
+
+describe("Testing stringHelpers", () => {
+
+    describe('testing snakeCaseToCamelCase function', function () {
+        it("should to throw Error", function () {
+            expect( string.snakeCaseToCamelCase ).toThrow( Error );
+        });
+
+        it("should to return camelCaseString", function () {
+            expect( string.snakeCaseToCamelCase("a") ).toBe( "a" );
+            expect( string.snakeCaseToCamelCase("test_string") ).toBe( "testString" );
+            expect( string.snakeCaseToCamelCase("long_test_string_") ).toBe( "longTestString" );
+            expect( string.snakeCaseToCamelCase("_a") ).toBe( "a" );
+            expect( string.snakeCaseToCamelCase("b_a") ).toBe( "bA" );
+        });
+    });
+
+});
+
