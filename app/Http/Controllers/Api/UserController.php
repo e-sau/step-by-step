904f2d9a7cfe1d16c7693e882c57da6db6f99bb8
esau <esau@yandex.ru>
2020-11-29 21:26:21 +0300

    Update UserController swagger-docs

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -128,14 +128,14 @@ class UserController extends Controller
      *      @OA\RequestBody(
      *          required=true,
      *          @OA\JsonContent(
-     *              @OA\Property(property="login", type="string"),
-     *              @OA\Property(property="name", type="string"),
-     *              @OA\Property(property="surname", type="string"),
-     *              @OA\Property(property="birthday", type="date"),
-     *              @OA\Property(property="email", type="string"),
-     *              @OA\Property(property="created_at", type="string"),
-     *              @OA\Property(property="password", type="string"),
-     *              @OA\Property(property="remember_token", type="string"),
+     *              required={"login", "name", "surname", "birthday", "email", "password", "password_confirmation"},
+     *              @OA\Property(property="login", type="string", example="user"),
+     *              @OA\Property(property="name", type="string", example="Alex"),
+     *              @OA\Property(property="surname", type="string", example="Murphy"),
+     *              @OA\Property(property="birthday", type="date", example="2014-01-01"),
+     *              @OA\Property(property="email", type="string", example="api@step-by-step.ru"),
+     *              @OA\Property(property="password", type="string", example="strongPass"),
+     *              @OA\Property(property="password_confirmation", type="string", example="strongPass"),
      *          ),
      *      ),
      *      @OA\Response(
